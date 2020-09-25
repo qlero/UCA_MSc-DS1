a5ec7bbefd8ef0c9db81e19db920d3d431bdf3b9
testGithubUCA <penseesaleatoires@gmail.com>
2020-09-25 21:58:47 +0200

    test ssh

---
+++ MSc 1 Refreshers/Basic Tools/hello_world.py
@@ -0,0 +1,11 @@
+def hello():
+    """this is a test function"""
+    print("This is a new message")
+    return None 
+
+def hello_new()
+    """this is another test function"""
+    print(f"{var}")
+
+if __name__ == "__main__":
+    hello()
