f703a63bbfe9cdf43872e39dda101a21bfdcf6d3
qlr <quentin.leroux@edhec.com>
2021-03-13 21:53:43 +0100

    stat learn homework draft done

--- 2_stat_learning/homework/part_2_application.py
+++ 2_stat_learning/homework/part_2_application.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_boston
+from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
 
 def load_boston_dataset(test_size, rd, show=True):
@@ -36,10 +37,10 @@ def print_boston_shapes(X_train, X_test, y_train, y_test):
           "Means of  feature columns (training set):",
           feature_means,
           sep="\n")
-    return None
 
 def format_x_non_centered(X):
     """
+    II.3
     Formats a data array as non-centered.
     """
     n = len(X)
@@ -61,19 +62,17 @@ def compute_w1(X_train, y_train, reg_parameter):
     X_t_Y = np.dot(X_t, y_train)
     return np.dot(inverse, X_t_Y)
 
-def predict_1(X_test, w1):
+def predict_1(X, w1):
     """
     II.4
     Predicts the targets of the non-centered test set given 
     a set of computed weights.
     """
-    n = len(X_test)
-    ones = np.ones((n,1))
-    X = np.concatenate([ones, X_test], axis=1)
-    return np.dot(X, w1)
+    return np.dot(format_x_non_centered(X), w1)
 
 def mean_squared_error(ground_truths, predictions):
     """
+    II.5
     Given a groundtruth array, and a prediction array, calculates
     a mean squared error score
     """
@@ -81,35 +80,127 @@ def mean_squared_error(ground_truths, predictions):
     n = len(ground_truths)
     for i in range(n):
         mse += (ground_truths[i]-predictions[i])**2
-    return mse
+    return mse/n
 
-def grid_search(split_range, reg_range, mode="non-centered"):
+def grid_search(splits, regs, mode="non-centered"):
     """
     II.5 
     Performs a grid search over a set of hyperparameters:
     Train-test split, regularization term
     """
     results = []
-    for split in split_range:
+    for split in splits:
         X_train, X_test, y_train, y_test = load_boston_dataset(split, 42, False)
-        X_test_final = np.transpose(format_x_non_centered(X_test))
-        for reg in reg_range:
+        for reg in regs:
             if mode=="non-centered": 
                 w = compute_w1(X_train, y_train, reg)
-            preds = np.dot(w, X_test_final)
+                preds = predict_1(X_test, w) 
+            else:
+                w, b = compute_w2(X_train, y_train, reg)
+                preds = predict_2(X_test, w, b)
             mse = mean_squared_error(y_test, preds)
             results.append([split, reg, w, preds, mse])
     return results
 
-def plot_grid_search(results, split):
+def plot_grid_search(results, splits, regs):
     """
+    II.5
     Plots the results of the grid search.
     """
-    formatted_results = {}
-    results = list(filter(lambda x: x[0]==split, results))
-    for result in results:
-        formatted_results[results[1]]=results[4]
-    return formatted_results
+    all_results = []
+    for split in splits:
+        results_iterator = list(filter(lambda x: x[0]==split, results))
+        plot_results = []
+        for result in results_iterator:
+            plot_results.append(result[4])
+        all_results.append([split, plot_results])
+        plt.plot(regs, plot_results, label=f"Test size: {split}", linewidth=3)
+    plt.xlabel("Regularization parameter value (Lambda)", fontsize=25)
+    plt.ylabel("MSE", fontsize=25)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.legend(fontsize=20)
+    plt.show()
+    all_results.sort(key=lambda x:np.mean(x[1]))
+    all_results = all_results[:3]
+    views = ["dotted", "dashdot", "dashed"]
+    for i, entry in enumerate(all_results):
+        plt.plot(regs, entry[1], linestyle=views[i], label=f"Test size: {entry[0]}", linewidth=3)
+    plt.xlabel("Regularization parameter value (Lambda)", fontsize=25)
+    plt.ylabel("MSE", fontsize=25)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.legend(fontsize=20)
+    plt.show()
+    return all_results
+
+def center_data(X):
+    """
+    II.8
+    Centers a non-centered dataset
+    """
+    return X-np.mean(X, axis=0)
+
+def compute_w2(X_train, y_train, reg_parameter):
+    """
+    II.8
+    Computes the weight matrix based on the non-centered train
+    set.
+    """
+    n = len(X_train)
+    d = len(X_train[0])
+    X = center_data(X_train)
+    X_t = np.transpose(X)
+    identity = np.identity(d)
+    inverse = np.linalg.inv(np.dot(X_t, X)+n*reg_parameter*identity)
+    X_t_Y = np.dot(X_t, y_train)
+    w2 = np.dot(inverse, X_t_Y)
+    b = np.mean(y_train)-np.dot(w2, np.mean(X, axis=0))
+    return w2, b
+
+def predict_2(X, w2, b):
+    """
+    II.8
+    Predicts the targets of the non-centered test set given 
+    a set of computed weights.
+    """
+    return np.dot(center_data(X), w2)+b
+
+def plot_predictions_vs_groundtruths(preds_w1, preds_w2, groundtruths):
+    """
+    II.9
+    Plots the predictions made with w1 and w2/b against a groundtruth.
+    """
+    plt.plot(groundtruths, label="Ground Truth", color="#f93949")
+    plt.plot(preds_w1, label="Predictions with non-centered data", color="#f4a460")
+    plt.plot(preds_w2, label="Predictions with centered data", color="#4fb57a")
+    plt.xlabel("Single Test Set Datapoint", fontsize=25)
+    plt.ylabel("Predicted Value (y_hat)", fontsize=25)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.legend(fontsize=20)
+    plt.show()
+
+def compare_w2_Ridge(preds_w1, X_train, y_train, X_test, y_test, reg):
+    """
+    II.11
+    Compares the predictions using the Ridge implementation of SKlearn
+    and the one used in II.8
+    """
+    model = Ridge(alpha=reg)
+    model.fit(X_train, y_train)
+    preds = model.predict(X_test)
+    plt.plot(y_test, label="Ground Truth", color="#f93949")
+    plt.plot(preds, label="Predictions with Sklearn Ridge Implementation", color="#f4a460")
+    plt.plot(preds_w1, label="Predictions with From Scratch Ridge Implementation (w1 method)", color="#4fb57a")
+    plt.xlabel("Single Test Set Datapoint", fontsize=25)
+    plt.ylabel("Predicted Value (y_hat)", fontsize=25)
+    plt.xticks(fontsize=18)
+    plt.yticks(fontsize=18)
+    plt.legend(fontsize=20)
+    plt.show()
+    mse = mean_squared_error(preds, preds_w1)
+    print(f"The MSE between the predictions computed with w1 and the Sklearn Ridge model is {mse}")
 
 if __name__ == "__main__":
     #II.1 load the Boston Dataset
@@ -129,17 +220,62 @@ if __name__ == "__main__":
           w1, sep="\n")
 
     #II.4 predicts based on w1
-    print("\n#### II.4 - Computing prediction with \hat{w}^1 ####")
-    preds = predict_1(X_test, w1)
-    print("Shape of predictions: ("+str(len(preds))+",)",
+    print("\n#### II.4 - Computing predictions with \hat{w}^1 ####")
+    preds_w1 = predict_1(X_test, w1)
+    print("Shape of predictions: ("+str(len(preds_w1))+",)",
           "First five predictions (using w1 computed in II.3):",
-          preds[:5],
+          preds_w1[:5],
           sep="\n")
 
     #II.5 GridSearch for w1
     print("\n#### II.5 - Performing a GridSearch for \hat{w}^1 ####")
-    reg_parameters = list(map(lambda x:x/100, (range(0,26))))
-    split_sizes = list(map(lambda x:x/100, (range(10, 42, 2))))
-    results = grid_search(split_sizes, reg_parameters)
-    for split in split_sizes:
-        formatted_results = plot_grid_search(results, split) 
+    split_sizes = list(map(lambda x:x/100, range(10, 42, 2)))
+    for divisor, max_range in [(10000, 1000), (1000**2, 1000)]:
+        reg_parameters = list(map(lambda x:x/divisor, range(0,max_range)))
+        results = grid_search(split_sizes, reg_parameters)
+        best_three_results = plot_grid_search(results, split_sizes, reg_parameters)
+    for entry in best_three_results:
+        param = reg_parameters[np.argmin(entry[1])]
+        print(f"For test split size {entry[0]}, the best MSE score was " + \
+              f"achieved with Lambda parameter: {param}.")
+
+    #II.8 computes w2, b, predicts based on w2, b
+    print("\n#### II.8 - Computing \hat{w}^2 and related predictions ####")
+    w2, b = compute_w2(X_train, y_train, 0.1)
+    print("Shape of w2: ("+str(len(w2))+",)",
+          "Example w2 computed with regularization parameter: 0.1",
+          w2, sep="\n")
+    print("Example Intercept computed with regularization parameter: 0.1",
+          b, sep="\n")
+    preds_w2 = predict_2(X_test, w2, b)
+    print("Shape of predictions: ("+str(len(preds_w2))+",)",
+          "First five predictions (using w2 and b):",
+          preds_w2[:5],
+          sep="\n")
+    print("\n#### Performing a GridSearch for \hat{w}^2 and b ####")
+    split_sizes = list(map(lambda x:x/100, range(10, 42, 2)))
+    for divisor, min_range, max_range in [(10000, 0, 1000), (1000**2, 0, 10000)]:
+        reg_parameters = list(map(lambda x:x/divisor, range(min_range, max_range)))
+        results = grid_search(split_sizes, reg_parameters, mode="centered")
+        best_three_results = plot_grid_search(results, split_sizes, reg_parameters)
+    
+    #II.9 Comparing the two approaches
+    print("\n#### II.9 - Computing the best 3 scenarions with \hat{w}^2 and b ####")
+    for entry in best_three_results:
+        param = reg_parameters[np.argmin(entry[1])]
+        print(f"For test split size {entry[0]}, the best MSE score was " + \
+              f"achieved with Lambda parameter: {param}.")
+
+    #II.9 compares the two approaches
+    plot_predictions_vs_groundtruths(preds_w1, preds_w2, y_test)
+    # Using results for train-test split 0.86/0.14
+    X_train, X_test, y_train, y_test = load_boston_dataset(0.14, 42, False)
+    w1 = compute_w1(X_train, y_train, 0.000249)
+    preds_w1 = predict_1(X_test, w1)
+    w2, b = compute_w2(X_train, y_train, 0.001861)
+    preds_w2 = predict_2(X_test, w2, b)
+    plot_predictions_vs_groundtruths(preds_w1, preds_w2, y_test)
+
+    #II.11 Comparing Ridge Sklearn and From Scratch implementation
+    print("\n#### II.11 - Comparing Sklearn Ridge with From Scratch implementation ####")
+    compare_w2_Ridge(preds_w1, X_train, y_train, X_test, y_test, 0.000249)
